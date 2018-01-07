e0d5ffcc88f69be049598351d87aa3d8c7dff1b2
Wilson Meng <w5meng@edu.uwaterloo.ca>
2018-01-07 15:55:06 -0500

    censor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDCjMteBE0_M3Y-1VXCjxkVctzaUSBAzMI',
-    authDomain: 'global-chat-a1e99.firebaseapp.com',
-    databaseURL: 'https://global-chat-a1e99.firebaseio.com',
-    projectId: 'global-chat-a1e99',
-    storageBucket: '',
-    messagingSenderId: '394159021987'
+    // security censor
   }
 };
